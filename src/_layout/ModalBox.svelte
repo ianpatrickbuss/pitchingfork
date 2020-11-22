1e858819222f6ecf6f8a8344f137ac98028a04e5
Ian Buss <ian@starcrusher.productions>
2020-11-22 15:06:41 -0500

    Added Modal component, moved landing components to own folder

---
+++ src/_layout/ModalBox.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  // Dependencies
+  import { createEventDispatcher } from "svelte";
+
+  // Props
+  export let hasClose: boolean = false;
+  // Component Events
+  const dispatch = createEventDispatcher();
+
+  const closeModal = () => {
+    dispatch("close");
+  };
+</script>
+
+<style lang="postcss">
+  .overlay {
+    @apply fixed bg-gray-500 opacity-50 h-screen w-screen inset-0 z-10;
+  }
+  main {
+    @apply absolute z-20 inset-0;
+    @apply flex justify-center align-middle items-center;
+  }
+  main div {
+    @apply mx-auto max-w-md;
+  }
+  main div article {
+    @apply bg-white opacity-100 mx-auto p-4 rounded-md inline-block;
+  }
+</style>
+
+<div
+  class="overlay md:fixed md:bg-gray-500 md:opacity-50 md:h-screen md:w-screen md:inset-0 md:z-10"
+  on:click={closeModal} />
+<main class="md:h-screen md:bg-transparent md:absolute md:z-20 md:inset-0">
+  <div class="fixed ">
+    {#if hasClose}
+      <button
+        class="right-0 md:-mr-4 md:-mt-4 rounded-sm absolute border-gray-600 bg-red-700 text-white px-3 py-1"
+        on:click={closeModal}>x</button>
+    {/if}
+    <article>
+      <slot />
+    </article>
+  </div>
+</main>
--- src/_layout/Tailwindcss.svelte
+++ src/_layout/Tailwindcss.svelte
@@ -4,12 +4,12 @@
   @tailwind utilities;
 
   .btn {
-    @apply rounded p-3 transition-all duration-200;
+    @apply rounded p-3 transition-all duration-200 text-white;
   }
 
   .btn.active,
   .btn.red {
-    @apply bg-red-700 text-white;
+    @apply bg-red-700;
   }
 
   .btn.active:hover,
@@ -18,39 +18,61 @@
   }
 
   .btn.indigo {
-    @apply bg-indigo-700 text-white;
+    @apply bg-indigo-700;
   }
 
   .btn.indigo:hover {
-    @apply bg-indigo-600 text-white;
+    @apply bg-indigo-600;
+  }
+
+  .btn.gray {
+    @apply bg-gray-600;
+  }
+
+  .btn.gray:hover {
+    @apply bg-gray-700;
+  }
+
+  .btn.light-gray {
+    @apply bg-gray-400;
+  }
+
+  .btn.light-gray:hover {
+    @apply bg-gray-500;
   }
 
   .btn.green {
-    @apply bg-green-700 text-white;
+    @apply bg-green-700;
   }
 
   .btn.green:hover {
-    @apply bg-green-600 text-white;
+    @apply bg-green-600;
   }
 
   .btn.gr-blue {
-    @apply bg-gradient-to-tl from-blue-600  to-blue-700 text-white;
+    @apply bg-gradient-to-tl from-blue-600  to-blue-700;
   }
+
   .btn.gr-blue:hover {
     @apply bg-gradient-to-br;
   }
+
   .btn.gr-pink {
-    @apply from-pink-600 bg-gradient-to-tl to-pink-700 text-white;
+    @apply from-pink-600 bg-gradient-to-tl to-pink-700;
   }
+
   .btn.gr-pink:hover {
     @apply bg-gradient-to-br;
   }
+
   .btn.gr-red {
     @apply from-red-800 to-red-700;
   }
+
   .btn.gr-indigo {
     @apply from-indigo-600 to-indigo-700 bg-gradient-to-tl;
   }
+
   .btn.gr-indigo:hover {
     @apply bg-gradient-to-br;
   }
